ac3e1cc71403d2ecff9d6b9662330f50c02e8ead
tuanbinhtran <tuanbinh.tran@icloud.com>
2020-02-21 17:48:19 +0100

    Add sample config

---
+++ config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    production: '',     // define production
+    prefix: '!',        // prefix for commands
+    token: '',          // your token
+    clientId: '',       // your client id
+    clientSecret: ''    // your client secret
+}
